3def2e819b22e081390835c5a244239060a312da
nz8 <60668100+nz8@users.noreply.github.com>
2021-03-14 18:09:23 -0500

    2 event setup scripts

---
+++ Assets/_Scripts/L1E1Setup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L1E1Setup : MonoBehaviour
+{
+    public GameObject sTarget;
+    public int tRadius=5;
+    public int tSpacing=3;
+
+    private int tHeight=20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTargets();
+    }
+
+    public void spawnTargets() {
+        int tandsDistance = tRadius * 2 * 10 + tSpacing * 9;
+        float startingPosition = -180+(360 - tandsDistance) / 2 + 5;
+        Vector3 pos = new Vector3(65, tHeight, startingPosition);
+        for (int i = 0; i < 10; i++) {
+            Instantiate(sTarget, pos, Quaternion.identity);
+            pos.z += 2*tRadius+tSpacing;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
