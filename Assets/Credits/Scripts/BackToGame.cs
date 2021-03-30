9e0914bcf2120fba393328ed44d66ac3b1ab1cb4
Tyler Goodwyn <tgoodwyn@gmail.com>
2021-03-29 21:20:57 -0500

    credits done. remember to add all scenes (3 of them) to the build settings before submitting

---
+++ Assets/Credits/Scripts/BackToGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToGame : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Scene switch code
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SceneManager.LoadScene("Scene 2/__Scene/Scene 2");
+        }
+    }
+}
